9765f51c980fb95cf155b1aa43bd7e4946291f7f
AmerAmora <Amer.m.amora@gmail.com>
2023-02-18 02:11:38 +0300

    Upload

--- Project8/Controllers/TransactionsController.cs
+++ Project8/Controllers/TransactionsController.cs
@@ -37,88 +37,22 @@ namespace Project8.Controllers
         }
 
         // GET: Transactions/Create
-        public ActionResult Create()
-        {
-            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email");
-            return View();
-        }
+       
 
         // POST: Transactions/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Transaction_Id,Transaction_Date,FullName,CardNumber,CVC,Visa_ExpireDate,User_Action,Amount,UserId")] Transaction transaction)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Transactions.Add(transaction);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", transaction.UserId);
-            return View(transaction);
-        }
+        
 
         // GET: Transactions/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Transaction transaction = db.Transactions.Find(id);
-            if (transaction == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", transaction.UserId);
-            return View(transaction);
-        }
-
+       
         // POST: Transactions/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Transaction_Id,Transaction_Date,FullName,CardNumber,CVC,Visa_ExpireDate,User_Action,Amount,UserId")] Transaction transaction)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(transaction).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", transaction.UserId);
-            return View(transaction);
-        }
-
-        // GET: Transactions/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Transaction transaction = db.Transactions.Find(id);
-            if (transaction == null)
-            {
-                return HttpNotFound();
-            }
-            return View(transaction);
-        }
-
-        // POST: Transactions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Transaction transaction = db.Transactions.Find(id);
-            db.Transactions.Remove(transaction);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        
+        
 
         protected override void Dispose(bool disposing)
         {
